9ef06db24d8257fa13c626e88311b889876dbf35
Kevin Lin <kevin.lin.p@gmail.com>
2021-02-09 22:09:31 -0500

    recruiter faq: start

--- src/build.sh
+++ src/build.sh
@@ -2,3 +2,7 @@ npx @11ty/eleventy --input=. --output=../
 rm -f ../resume.html
 mv ../resume/index.html ../resume.html
 rm -r ../resume/
+
+rm -f ../recruiter-faq.html
+mv ../recruiter-faq/index.html ../recruiter-faq.html
+rm -r ../recruiter-faq/
